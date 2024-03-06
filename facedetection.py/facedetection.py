de288d03344b6dfe864d6249658264d245bb64b2
Vanshika <vanshikasrivastava2302@gmail.com>
2024-03-06 14:42:38 +0530

    Initial Commit

--- facedetection.py/facedetection.py
+++ facedetection.py/facedetection.py
@@ -4,24 +4,19 @@ import numpy as np
 import csv
 from datetime import datetime
 
-video_capture=cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
+elone_image = face_recognition.load_image_file("elone.jpg")
+elone_encoding = face_recognition.face_encodings(elone_image)[0]
 
-k1_image=face_recognition.load_image_file("k1.jpg")
-k1_encoding=face_recognition.face_encodings(k1_image)[0]
+kal_image = face_recognition.load_image_file("kal.jpg")
+kal_encoding = face_recognition.face_encodings(kal_image)[0]
 
-kalpana_image=face_recognition.load_image_file("kalpana.jpg")
-kalpana_encoding=face_recognition.face_encodings(kalpana_image)[0]
+known_face_encodings = [elone_encoding, kal_encoding]
+known_face_names = ["elone musk", "Kalpana Chawla"]
 
-known_face_encodings = [k1_image,kalpana_image]
-
-known_face_names = ["kalpana Chawla","Kalpana Chaudhary"]
-
-#list of expect.. stud
-
+# list of expected students
 great = known_face_names.copy()
-face_locations=[]
-face_encodings=[]
 
 # current time , date
 now = datetime.now()
@@ -31,39 +26,38 @@ f = open(f"{current_date}.csv", "w+", newline="")
 lnwriter = csv.writer(f)
 
 while True:
-  _,frame=video_capture.read()
-  small_frame=cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-  rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
-  #recognize faces
+    # recognize faces
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-  face_locations = face_recognition.face_locations(rgb_small_frame)
-  face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+    if face_encodings:
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            face_distance = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distance)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-  for face_encoding in face_encodings:
-     matches = face_recognition.compare_faces(known_face_encodings,face_encoding )
-     face_distance =face_recognition.face_distance(known_face_encodings,face_encoding)
-     best_match_index = np.argmin(face_distance)
-     if(matches[best_match_index]):
-        name = known_face_names[best_match_index]
+                if name:
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    bottomLeftCornerOfText = (10, 100)
+                    fontScale = 1.5
+                    fontColor = (255, 0, 0)
+                    thickness = 3
+                    lineType = 2
+                    cv2.putText(frame, name + " Present", bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+                    if name in great:
+                        great.remove(name)
+                        current_time = now.strftime("%H:%M:%S")
+                        lnwriter.writerow([name, current_time])
 
-     if name in known_face_names:
-        font=cv2.FONT_HERSHEY_SIMPLEX
-        bottomLeftCornerOfText=(10,100)
-        fontScale=1.5
-        fontColor=(255,0,0)
-        thickness=3
-        lineType=2
-        cv2.putText(frame, name  +  "Present", bottomLeftCornerOfText,font,fontScale, fontColor, thickness, lineType)
-     if name in great:
-        great.remove(name)
-        current_time=now.strftime("%H:%M%S")
-        lnwriter.writerow([name,current_time])
-
-  cv2.imshow("attendence", frame)
-  if cv2.waitKey(1) & 0xFF == ord("q"):
-     break
-     
+    cv2.imshow("attendance", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
 video_capture.release()
 cv2.destroyAllWindows()
